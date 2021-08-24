8fa0f2c385d4ef2aa92843ca6f43126a5949bb29
aaronrosati <84755343+aaronrosati@users.noreply.github.com>
2021-08-23 19:25:32 -0700

    Add files via upload

---
+++ precache-manifest.d75200d163088337601f4e9c72aa1a06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fde477201c22acbcdb2af132789687a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07996cea11caf00878f0",
+    "url": "/static/js/2.30916d2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.30916d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f717667d077ba407d778",
+    "url": "/static/js/main.3ce7ed5f.chunk.js"
+  },
+  {
+    "revision": "aa7c612222590812d412",
+    "url": "/static/js/runtime-main.1160daa0.js"
+  }
+]);
